108ad39ce4a2dc4667b2637a80fd4a68e1f351ef
Becky <rebecca.loveking@gmail.com>
2016-06-02 15:17:19 -0400

    changed text back to fix

--- src/main/java/com/mycompany/MavenTest4Jenkins/App.java
+++ src/main/java/com/mycompany/MavenTest4Jenkins/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.ouy.println( "The standard thing to say here is: Hello World!" );
+        System.out.println( "The standard thing to say here is: Hello World!" );
     }
 }
