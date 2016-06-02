9400d76d7e488f2e46f3093afec457a13bf0bbe2
Becky <rebecca.loveking@gmail.com>
2016-06-02 14:53:30 -0400

    changed text

--- src/main/java/com/mycompany/MavenTest4Jenkins/App.java
+++ src/main/java/com/mycompany/MavenTest4Jenkins/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "The standard thing to say here is: Hello World!" );
     }
 }
